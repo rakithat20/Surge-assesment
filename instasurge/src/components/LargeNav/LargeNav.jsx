058f38e603027f38dc867c18134d3c7176120f3a
Rakitha <tharupathir@gmail.com>
2025-01-08 12:46:50 +0530

    Side Nav Finished
    Side nav bar for bigger screens finished

--- instasurge/src/components/LargeNav/LargeNav.jsx
+++ instasurge/src/components/LargeNav/LargeNav.jsx
@@ -1,6 +1,7 @@
 import SearchLogo from "../../../src/assets/navlogo/search.png"
 import ExploreLogo from "../../../src/assets/navlogo/explore.png"
 import ReelsLogo from "../../../src/assets/navlogo/reel.png"
+import HomeLogo from "../../../src/assets/navlogo/home.png"
 import MessagesLogo from "../../../src/assets/navlogo/message.png"
 import NotificationsLogo from "../../../src/assets/navlogo/like.png"
 import CreateLogo from "../../../src/assets/navlogo/create.png"
@@ -48,6 +49,34 @@ const LargeNav = () => {
         <Link to='/' className="mb-10 px-2 lg:block md:hidden sm:hidden hidden ">
             <img src={InstagramLogo} alt="instagram logo" className="img w-28 h-auto"></img>
         </Link>
+        <Link to='/' className="mb-10 px-2 lg:hidden md:block sm:block block ">
+            <img src={InstagramIcon} alt="instagram logo" className="img w-10 h-auto"></img>
+        </Link>
+        <div className="w-full h-auto flex items-start flex-col gap-y-2">
+            <Link to='/' className="w=full h-auto flex items-center gap-x-4 p-3 bg-transparent hover:bg-gray-800/60 rounded-md
+             ease-out duration-500 group">
+                <img src={HomeLogo} alt="Home icon" className="w-6 h-6 object-contain group-hover:scale-105 ease-out duration-300" />
+                <p className="text-base font-semibold text-white lg:block md:hidden sm:hidden ">Home</p>
+            </Link>
+            {sidebarItems.map((item)=>(<Link key={item.name}
+                to={item.link}
+                className="w=full h-auto flex items-center gap-x-4 p-3 bg-transparent hover:bg-gray-800/60 rounded-md
+                ease-out duration-500 group">
+                   <img src={item.icon} alt="Home icon" className="w-6 h-6 object-contain group-hover:scale-105 ease-out duration-300" />
+                   <p className="text-base font-semibold text-white lg:block md:hidden sm:hidden ">{item.name}</p>
+               </Link>))}
+               <Link 
+                to='/profile'
+                className="w=full h-auto flex items-center gap-x-4 p-3 bg-transparent hover:bg-gray-800/60 rounded-md
+                ease-out duration-500 group">
+                   <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBBSFEB9Aq8f79o35ql5gbX4Y48oiWwBhYKg&s' alt="profile icon" className="w-6 h-6 rounded-full object-cover group-hover:scale-105 ease-out duration-300" />
+                  
+                  
+                   {/*need to fill from the backend*/}
+                   <p className="text-base font-semibold text-white lg:block md:hidden sm:hidden ">Profile</p>
+               </Link>
+        </div>
+
     </div>
   )
 }
