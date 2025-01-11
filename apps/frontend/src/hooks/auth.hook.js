ab6da47d4e7ab0529d162a4485c1c4eb111f397c
Rakitha <tharupathir@gmail.com>
2025-01-11 23:29:23 +0530

    Update auth.hook.js

--- apps/frontend/src/hooks/auth.hook.js
+++ apps/frontend/src/hooks/auth.hook.js
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/Authcontext";
-// Custom hook to access authentication context
 export const useAuth = () => {
-  return useContext(AuthContext); // Return value from context
+  return useContext(AuthContext);
 };
